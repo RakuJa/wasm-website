0ae28bc51faa5f5f9ccfdaeac81a1ee1f54821b3
rakuja <danielegiachetto1999@gmail.com>
2025-07-27 21:50:11 +0200

    chore: update build

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-set -euo pipefail
-export HOME=/root
-
-# Install Rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -t wasm32-unknown-unknown --profile minimal
-
 cargo install --locked trunk
 
 # Build project with trunk
