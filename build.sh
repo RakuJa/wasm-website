ee17a94dc9aea22aa74afb5a4c2f9ad7a36c6ed8
rakuja <danielegiachetto1999@gmail.com>
2025-07-27 21:56:37 +0200

    chore: after installing rust, point to cargo

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # Install Rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -t wasm32-unknown-unknown --profile minimal
 
+. "$HOME/.cargo/env"
+
 cargo install --locked trunk
 
 # Build project with trunk
